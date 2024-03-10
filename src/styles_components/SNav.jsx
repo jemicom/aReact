d7c789515b9bda56a8a49f71c97794f7179a3182
jemicom <jemicom@gmail.com>
2024-03-10 17:50:52 +0900

    nav 이벤트 사용법 추가

--- src/styles_components/SNav.jsx
+++ src/styles_components/SNav.jsx
@@ -1,17 +1,59 @@
 import React from "react";
 
+// 파라미터 유무
+// 파라미터가 있다면 ()=>{}를 사용
+// 파라미터가 없다면 그냥 호출
+
 const SNav = () => {
-  return <nav className="lnb">
-    <ul>
-      <li><a href="#">HOME</a></li>
-      <li><a href="#">HTML</a></li>
-      <li><a href="#">CSS</a></li>
-      <li><a href="#">JAVASCRIPT</a></li>
-      <li><a href="#">jQuery</a></li>
-      <li><a href="#">nodejs</a></li>
-      <li><a href="#">ABOUT</a></li>
-    </ul>
-  </nav>;
+  const onMouseHoverHandle = () => {
+    console.log("mouse Over");
+  };
+  const onMousOutHandle = () => {
+    console.log("mouse Out");
+  };
+  return (
+    <nav className="lnb">
+      <ul>
+        <li>
+          <a
+            href="#"
+            onMouseOver={() => {
+              console.log("마우스 오버");
+            }}
+            onMouseOut={() => {
+              console.log("마우스 아웃");
+            }}
+          >
+            HOME
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onMouseOver={onMouseHoverHandle}
+            onMouseOut={onMousOutHandle}
+          >
+            HTML
+          </a>
+        </li>
+        <li>
+          <a href="#">CSS</a>
+        </li>
+        <li>
+          <a href="#">JAVASCRIPT</a>
+        </li>
+        <li>
+          <a href="#">jQuery</a>
+        </li>
+        <li>
+          <a href="#">nodejs</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default SNav;
